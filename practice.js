3e3190bad5f329b0584290410e204186087a30a0
aanya2707 <85337689+aanya2707@users.noreply.github.com>
2022-01-04 12:34:05 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAp9L3fRwTFUtxzTEac_E7PYVb8rmMO4L4",
+    authDomain: "activity-35511.firebaseapp.com",
+    databaseURL: "https://activity-35511-default-rtdb.firebaseio.com",
+    projectId: "activity-35511",
+    storageBucket: "activity-35511.appspot.com",
+    messagingSenderId: "5605442125",
+    appId: "1:5605442125:web:5092c9320d12528a8a9204"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//ADD YOUR FIREBASE LINKS
+
+function addUser(){
+    username = document.getElementById("add_user").value;
+    firebase.database().ref("/").child(username).update({
+        purpose:"add user"
+    });
+}
